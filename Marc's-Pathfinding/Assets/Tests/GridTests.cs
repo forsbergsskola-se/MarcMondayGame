84eb756d997b54f439268eefdd3f00cae3c1a627
xgabrielle <gabrielle.bergman@student.forsbergsskola.se>
2024-05-21 14:23:54 +0200

    add unit test

---
+++ Marc's-Pathfinding/Assets/Tests/GridTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections;
+
+namespace Tests
+{
+    public class GridTests
+    {
+        [Test]
+        public void GridTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions.
+            
+        }
+
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        [UnityEngine.TestTools.UnityTest]
+        public IEnumerator GridTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // yield to skip a frame
+            yield return null;
+        }
+    }
+}
